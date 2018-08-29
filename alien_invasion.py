f96b486aa14de927a213c17a92ee3d8d9d534bb8
chimingbei <chimingbei@126.com>
2018-08-29 21:01:09 +0800

    add settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,11 +6,13 @@ Create by 2018.08.28
 '''
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     #设置背景颜色
@@ -24,7 +26,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # 每次循环时都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
                
         # 让最近绘制的屏幕看见
         pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#_*_ coding:utf-8 _*_
+'''
+Create by 2018.08.29
+@author:Marco.Chi
+'''
+class Settings():
+    #存储在《外星人入侵》的所有设置的类
+    def __init__(self):
+        #初始化设置
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
